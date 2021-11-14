6cd5b3b7ed4139e5a48adbce8484c2edb8e1ae65
Sl1zerin <83447264+Sl1zerin@users.noreply.github.com>
2021-11-15 00:27:05 +0300

    1
    1

---
+++ Sl1z/Scripts/ChangeLocol.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLocol : MonoBehaviour
+{
+    
+    public bool stay;
+    public Transform point;
+    public GameObject nextRoom;
+    public GameObject lastRoom;
+
+    public GameObject character;
+    public InputCharacter inputCharacter;
+
+    public AudioSource audioDoor;
+
+    public bool changeroom = false;
+
+    public SettingOptionsCamera settingText;
+
+    public GameObject Text;
+
+    private void OnTriggerEnter(Collider other) {
+        stay = true;
+        if(other.tag == "Character")
+        {
+            character = other.gameObject;
+            inputCharacter=character.GetComponent<InputCharacter>();
+        }
+    }
+    private void OnTriggerStay(Collider other) {
+        stay = true;
+        if(other.tag == "Character")
+        {
+            character = other.gameObject;
+            inputCharacter=character.GetComponent<InputCharacter>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        stay = false;
+        if(other.tag == "Character")
+        {
+            character = null;
+            inputCharacter= null;
+        }
+    }
+
+    void Update()
+    {
+        UpdateInput();
+        UpdateChange();
+    }
+
+    void UpdateInput()
+    {
+        if(stay){
+            Text.SetActive(true);    
+            if(inputCharacter != null && inputCharacter.actionEvent){
+                changeroom = true;
+            }
+        }
+        else Text.SetActive(false);
+    }
+
+    void UpdateChange()
+    {
+            if(changeroom){
+                audioDoor.Play();
+                nextRoom.SetActive(true);
+                character.transform.position = point.position;
+                lastRoom.SetActive(false);
+                character = null;
+                inputCharacter = null;
+                stay = false;
+                changeroom = false;
+                Debug.Log("change");
+            }
+    }
+}
---
+++ Sl1z/Scripts/LampTrigger.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampTrigger : MonoBehaviour
+{   
+    [Header("Переменные для включения лапы")]
+    #region Вкл.Выкл лампы
+    public bool Stay;
+    public GameObject[] lampLight;
+    public GameObject Text;
+    public bool active;
+    public string nameObject;
+    public InputCharacter inputCharacter;
+
+    private bool open_closet = false;
+
+    private float timeOut;
+
+    #endregion
+
+    [Header("Переменные для коректного расположения теста объекта")]
+    #region 
+    public SettingOptionsCamera settingText;
+
+    #endregion
+
+    #region Работа со звуком
+
+    public AudioSource audioOpen;
+
+    #endregion
+
+
+    #region Определения нахождения объекта в триггере
+    public GameObject character;
+    private void OnTriggerEnter(Collider other) {
+        Stay=true;
+        if(other.tag == "Character") 
+        {
+            character = other.gameObject;
+            inputCharacter=character.GetComponent<InputCharacter>();
+        }
+    }
+    private void OnTriggerStay(Collider other) {
+        Stay=true;
+        if(other.tag == "Character") 
+        {
+            character = other.gameObject;
+            inputCharacter=character.GetComponent<InputCharacter>();
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        Stay=false;
+        if(other.tag == "Character") 
+        {
+            character = null;
+            inputCharacter=null;
+        }
+    }
+
+    #endregion
+    void Start() {
+        AwakeText();
+    }
+
+    void  AwakeText()
+    {
+        for (int i = 0; i < lampLight.Length; i ++)
+        {
+            lampLight[i].SetActive(active);
+        }
+        //Text.transform.rotation= Quaternion.Euler(settingText.RotX,settingText.RotY,settingText.RotZ);
+    }
+    void Update()
+    {
+        
+    ActionsObject();
+    ActiveLight();
+    }
+
+    void ActionsObject()
+    {
+        if (Stay == true ){
+            Text.SetActive(true);
+            if (inputCharacter.actionEvent && !open_closet){
+                open_closet = true;
+            
+            }
+        }
+        else Text.SetActive(false);
+    }
+
+    void ActiveLight()
+    {
+        if(open_closet){
+            timeOut += Time.deltaTime;
+            if(timeOut < 0.3)
+            {
+                if (active==true){
+                    audioOpen.Play();
+                    for (int i = 0; i < lampLight.Length; i ++)
+                    {
+                        lampLight[i].SetActive(false);
+                    }   
+                    active=false;
+                    open_closet = false;
+                }
+                else if (active==false){
+                    audioOpen.Play();
+                    for (int i = 0; i < lampLight.Length; i ++)
+                    {
+                        lampLight[i].SetActive(true);
+                    }
+                    active=true;
+                    open_closet = false;
+                }
+            }
+            else {
+                open_closet = false;
+                timeOut = 0;
+            }
+        }
+    }
+}
